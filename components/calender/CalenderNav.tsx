10c474c50dd390fd7df5cb41af261c3fde71beb4
Lucid Kobold <lucid@lucidcreations.media>
2021-11-30 21:26:51 -0600

    Updated to use context.

--- components/calender/CalenderNav.tsx
+++ components/calender/CalenderNav.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Heading, HStack, IconButton } from "@chakra-ui/react";
 import { Icon } from "@iconify/react";
 import { format } from "date-fns";
+import { CalenderContext } from "../../contexts/CalenderContext";
 
 const CalenderNav = (): JSX.Element => {
-  const today = new Date();
+  const { today } = useContext(CalenderContext);
 
   const currentMonth = format(today, "LLLL uuuu");
 
