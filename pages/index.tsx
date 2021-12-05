c61f03f4e333e10e7c92a2a2242236f2a23e9493
Lucid Kobold <lucid@lucidcreations.media>
2021-12-05 16:48:34 -0600

    Temp hard code a date into the calander component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
-import Calender from "../components/calender/Calender";
+import Calender from "../components/calender";
 import { CalenderContextProvider } from "../contexts/CalenderContext";
 
 const IndexPage = (): JSX.Element => {
   return (
     <Box textAlign="center" w="100%" h="auto" pt="50px" pb="10vh">
       <CalenderContextProvider>
-        <Calender />
+        <Calender date={{ year: 2021, month: 12, day: 1 }} />
       </CalenderContextProvider>
     </Box>
   );
