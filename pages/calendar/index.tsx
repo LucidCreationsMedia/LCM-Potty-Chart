2ca6bab953d7438e88fa501e533c89ae5c6fda11
Lucid Kobold <lucid@lucidcreations.media>
2021-12-05 16:47:26 -0600

    Added dynamic calender route.

---
+++ pages/calendar/index.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import { Box, Heading } from "@chakra-ui/react";
+
+const DateRoute = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router) {
+      router.push("calendar/now");
+    }
+  }, [router]);
+
+  return (
+    <Box textAlign="center" w="100%" h="auto" pt="50px" pb="10vh">
+      <Heading as="h2" size="xl">
+        Loading
+      </Heading>
+    </Box>
+  );
+};
+
+export default DateRoute;
