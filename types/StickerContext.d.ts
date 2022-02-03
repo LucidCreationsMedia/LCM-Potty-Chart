a213b27eebce88586c94cde5278664c52a4f5132
Lucid Kobold <lucid@lucidcreations.media>
2022-02-03 13:38:49 -0600

    Add sticker interfaces and types.

--- types/StickerContext.d.ts
+++ types/StickerContext.d.ts
@@ -1,3 +1,28 @@
 interface StickersContextState {
   stickersMonth: StickerDays;
+  addEditSticker: (date: Date, sticker: ValidStickerVal) => void;
+}
+
+type StickerVal = -2 | -1 | 0 | 1 | 2 | null;
+
+type ValidStickerVal = -2 | -1 | 0 | 1 | 2;
+
+interface AddEditStickerProps {
+  date: Date;
+  sticker: ValidStickerVal;
+}
+
+interface Sticker {
+  id: string;
+  date: Date;
+  sticker: StickerVal;
+  edited: boolean;
+  manual: boolean;
+}
+
+type StickerDays = Sticker[];
+
+interface MonthSticker {
+  date: Date;
+  month: Sticker[];
 }
