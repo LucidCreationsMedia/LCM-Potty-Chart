ebfe40860c0878054f2a0c322b8b1c64bb559932
Lucid Kobold <lucid@lucidcreations.media>
2022-02-14 17:20:39 -0600

    Update app version

--- theme/layout/Header.tsx
+++ theme/layout/Header.tsx
@@ -15,7 +15,7 @@ import appLogo from "../../public/images/logo.svg";
 
 const Header = (): JSX.Element => {
   const appName = "LCM Potty Chart";
-  const appVersion = "v0.0.7.0-alpha";
+  const appVersion = "v0.0.7.2-alpha";
 
   // Add transparency while not at the top of the page.
   const [transparentNavbar, setTransparentNavbar] = useState<boolean>(false);
