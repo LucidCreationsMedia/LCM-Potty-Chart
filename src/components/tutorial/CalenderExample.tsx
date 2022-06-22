ba4823334e1a68b92724b33dc150aeb7e820b961
Lucid Kobold <72232219+LucidKobold@users.noreply.github.com>
2022-06-22 13:05:55 -0500

    added todos

--- src/components/tutorial/CalenderExample.tsx
+++ src/components/tutorial/CalenderExample.tsx
@@ -12,6 +12,11 @@ interface CalenderExampleProps {
 const CalenderExample = ({
   type
 }: CalenderExampleProps): JSX.Element => {
+  // TODO: Check if the current date is the start of the user's preferred start of the week and use the previous week for the edit example.
+
+  // TODO: Add highlight to the current date for the add example and highlight other dates when the edit example is used.
+
+  // TODO: Disable the days that shouldn't have a function to prevent edits on add example and add to current date on the edit example.
 
   const currDateObj: Date = new Date();
   const isLoading = false;
