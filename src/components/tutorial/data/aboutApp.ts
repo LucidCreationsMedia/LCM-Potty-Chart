4985fc90421aa5b111325ef44b633b3bda9746f2
Lucid Kobold <72232219+LucidKobold@users.noreply.github.com>
2022-06-24 15:36:04 -0500

    Fixed grammer.

--- src/components/tutorial/data/aboutApp.ts
+++ src/components/tutorial/data/aboutApp.ts
@@ -1,7 +1,7 @@
 type AboutApp = string[];
 
 const aboutApp: AboutApp = [
-  "The Potty Chart is app that mimics a potty/star chart commonly used while potty training toddler or child.",
+  "The Potty Chart is an app that mimics a potty/star chart commonly used while potty training toddler or child.",
   "The app can be used to track behavior, habits, diaper training, potty training (good luck), daily chores/tasks, or anything else you might want to track in a fun and visual way with colorful themes, stickers, and even receive encouraging messaged from your big/dom, followed, and friends.",
   "The final app will have settings to disable any mentions and references of ABDL to allow a more general audience to use, such as for a master and pet relationship.",
   "This is a beta build of the app. Some functionality may not work as intended, is not fully functional, and may be missing entirely."
